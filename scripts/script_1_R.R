44b97c06f8b8bb03832b0766deea1420cb345a36
Chivolta <jacekkurus@gmail.com>
2020-03-03 12:46:42 +0100

    Add files via upload

---
+++ scripts/script_1_R.R
@@ -0,0 +1,13 @@
+# libraries
+library(tm)
+
+work_dir <- "D:\\JK - WZISS2-2412IS\\PJN"
+setwd(work_dir)
+
+input_dir <- ".\\data"
+
+output_dir <- ".\\results"
+
+scripts_dir <- ".\\scripts"
+
+dir.create(output_dir)
